7dc09fcd0cd49ca524dcbebca1b2f199a05955d5
GautamDhiman <gautamCodesHere@gmail.com>
2023-05-19 12:30:28 +0530

    Add Vehicle type
    What?
    - Car

---
+++ Class/car.py
@@ -0,0 +1,7 @@
+from Class import Vehicle
+
+class Car(Vehicle):
+
+    def __init__(self, identifier):
+        super.__init__(identifier, 'Car')
+
---
+++ Class/vehicle.py
@@ -0,0 +1,7 @@
+
+class Vehicle:
+
+    def __init__(self, identifier, vehicle_type):
+        self.identifier = identifier
+        self.vehicle_type = vehicle_type
+
